dfba43cd25ef1c5902a94e256aa8293ba7a4829b
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-05-23 22:26:15 +0300

    Initial commit

---
+++ DistinctWordCounterLibrary/DistinctWordCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DistinctWordCounterLibrary;
+
+public class DistinctWordCounter
+{
+    private Dictionary<string, int> GetWordsQuantity(string fileContent)
+    {
+        var finalDictionary = new Dictionary<string, int>();
+        fileContent = Regex.Replace(fileContent, "<[^>]+>", string.Empty);
+        fileContent = Regex.Replace(fileContent, "[^a-zA-Zа-яА-я'\\s]", string.Empty);
+        fileContent = Regex.Replace(fileContent, "[\n\t\r]", string.Empty);
+        Console.WriteLine("Файл найден!\nИдет подсчёт слов...");
+        var words = fileContent
+            .Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries)
+            .GroupBy(o => o.ToLower())
+            .OrderByDescending(o => o.Count());
+        var finalstr = string.Empty;
+        foreach (var word in words)
+        {
+            finalDictionary.Add(word.Key,word.Count());
+        }
+
+        return finalDictionary;
+    }
+}
---
+++ ReflectionConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+Console.Write("Введите путь к текстовому файлу (прим: C:\\Users\\Skylice\\Desktop\\voina.fb2): ");
+var path = Console.ReadLine();
+var text = string.Empty;
+if (File.Exists(path))
+{
+    text = File.ReadAllText(path, Encoding.UTF8);
+    Assembly assembly = Assembly.LoadFrom("DistinctWordCounterLibrary.dll");
+    var type = assembly.GetType("DistinctWordCounterLibrary.DistinctWordCounter");
+    var methodInfo = type.GetMethod("GetWordsQuantity",BindingFlags.NonPublic | BindingFlags.Instance);
+    var instance = Activator.CreateInstance(type);
+    var resultDictionary = methodInfo.Invoke(instance, new object[] { text }) as Dictionary<string,int>;
+    var curDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+    var finPath = curDir.FullName.Replace(curDir.Name, string.Empty) + $"{Path.GetFileName(path)
+        .Split(new[] { ".txt", ".fb2", ".docx", ".doc", ".rtf" }, StringSplitOptions.RemoveEmptyEntries)[0]}_words.txt";
+    File.WriteAllLines(finPath, resultDictionary.Select(x=> x.Key + ": " + x.Value).ToArray());
+    Console.WriteLine($"Готово!\nМесторасположение созданного файла: {finPath}");
+    Process.Start("explorer.exe", finPath);
+}
+else
+{
+    Console.WriteLine("Файл не найден!");
+}
+Console.ReadLine();
