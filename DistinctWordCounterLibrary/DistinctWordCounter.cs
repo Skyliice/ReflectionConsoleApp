efcc4f0e45396c9c78723f1e74908652c0141638
C-INFORM\metelitsa <b.metelitsa@center-inform.ru>
2023-05-26 11:30:46 +0300

    Добавлен метод отвечающий за параллельный подсчет слов

--- DistinctWordCounterLibrary/DistinctWordCounter.cs
+++ DistinctWordCounterLibrary/DistinctWordCounter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
 
 namespace DistinctWordCounterLibrary;
 
@@ -15,12 +16,31 @@ public class DistinctWordCounter
             .Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries)
             .GroupBy(o => o.ToLower())
             .OrderByDescending(o => o.Count());
-        var finalstr = string.Empty;
         foreach (var word in words)
         {
             finalDictionary.Add(word.Key,word.Count());
         }
-
         return finalDictionary;
+    }
+
+    private ConcurrentDictionary<string, int> finalDictionary;
+    public ConcurrentDictionary<string,int> GetWordsQuantityInParallel(string fileContent)
+    {
+        finalDictionary = new ConcurrentDictionary<string, int>();
+        fileContent = Regex.Replace(fileContent, "<[^>]+>", string.Empty);
+        fileContent = Regex.Replace(fileContent, "[^a-zA-Zа-яА-я'\\s]", string.Empty);
+        fileContent = Regex.Replace(fileContent, "[\n\t\r]", string.Empty);
+        Console.WriteLine("Файл найден!\nИдет подсчёт слов...");
+        var words = fileContent
+            .Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries)
+            .GroupBy(o => o.ToLower())
+            .OrderByDescending(o => o.Count()).AsParallel().ToArray();
+        Parallel.ForEach(words, DictionaryTryAdd);
+        return finalDictionary;
+    }
+
+    private void DictionaryTryAdd(IGrouping<string,string> grouping)
+    {
+        finalDictionary.TryAdd(grouping.Key, grouping.Count());
     }
 }
--- ReflectionConsoleApp/Program.cs
+++ ReflectionConsoleApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
+using DistinctWordCounterLibrary;
 
 Console.Write("Введите путь к текстовому файлу (прим: C:\\Users\\Skylice\\Desktop\\voina.fb2): ");
 var path = Console.ReadLine();
 var text = string.Empty;
+var time = string.Empty;
 if (File.Exists(path))
 {
     text = File.ReadAllText(path, Encoding.UTF8);
@@ -12,7 +15,17 @@ if (File.Exists(path))
     var type = assembly.GetType("DistinctWordCounterLibrary.DistinctWordCounter");
     var methodInfo = type.GetMethod("GetWordsQuantity",BindingFlags.NonPublic | BindingFlags.Instance);
     var instance = Activator.CreateInstance(type);
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
     var resultDictionary = methodInfo.Invoke(instance, new object[] { text }) as Dictionary<string,int>;
+    stopwatch.Stop();
+    time = $"Usual method: {stopwatch.Elapsed.Minutes}:{stopwatch.Elapsed.Seconds}:{stopwatch.Elapsed.Milliseconds}";
+    var asyncInstance = new DistinctWordCounter();
+    stopwatch.Restart();
+    var parallDict = asyncInstance.GetWordsQuantityInParallel(text);
+    stopwatch.Stop();
+    time = time + $"\tMethodInParallel: {stopwatch.Elapsed.Minutes}:{stopwatch.Elapsed.Seconds}:{stopwatch.Elapsed.Milliseconds}";
+    Console.WriteLine(time);
     var curDir = new DirectoryInfo(Directory.GetCurrentDirectory());
     var finPath = curDir.FullName.Replace(curDir.Name, string.Empty) + $"{Path.GetFileName(path)
         .Split(new[] { ".txt", ".fb2", ".docx", ".doc", ".rtf" }, StringSplitOptions.RemoveEmptyEntries)[0]}_words.txt";
