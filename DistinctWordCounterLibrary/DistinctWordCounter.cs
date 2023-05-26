71dba35a7a53f126e0769798619196cfb3c3e0b5
C-INFORM\metelitsa <b.metelitsa@center-inform.ru>
2023-05-26 12:06:51 +0300

    Some changes

--- DistinctWordCounterLibrary/DistinctWordCounter.cs
+++ DistinctWordCounterLibrary/DistinctWordCounter.cs
@@ -22,25 +22,22 @@ public class DistinctWordCounter
         }
         return finalDictionary;
     }
-
-    private ConcurrentDictionary<string, int> finalDictionary;
+    
     public ConcurrentDictionary<string,int> GetWordsQuantityInParallel(string fileContent)
     {
-        finalDictionary = new ConcurrentDictionary<string, int>();
+        var finalDictionary = new ConcurrentDictionary<string, int>();
         fileContent = Regex.Replace(fileContent, "<[^>]+>", string.Empty);
         fileContent = Regex.Replace(fileContent, "[^a-zA-Zа-яА-я'\\s]", string.Empty);
         fileContent = Regex.Replace(fileContent, "[\n\t\r]", string.Empty);
         Console.WriteLine("Файл найден!\nИдет подсчёт слов...");
         var words = fileContent
-            .Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries)
+            .Split(new[] { ",", ";", " " }, StringSplitOptions.RemoveEmptyEntries).AsParallel()
             .GroupBy(o => o.ToLower())
-            .OrderByDescending(o => o.Count()).AsParallel().ToArray();
-        Parallel.ForEach(words, DictionaryTryAdd);
+            .OrderByDescending(o => o.Count());
+        foreach (var word in words)
+        {
+            finalDictionary.TryAdd(word.Key,word.Count());
+        }
         return finalDictionary;
-    }
-
-    private void DictionaryTryAdd(IGrouping<string,string> grouping)
-    {
-        finalDictionary.TryAdd(grouping.Key, grouping.Count());
     }
 }
